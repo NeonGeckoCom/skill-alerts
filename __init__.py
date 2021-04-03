04c5048ec6c704feb1e13c0e35496e779f93ede4
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2021-04-02 17:28:11 -0700

    Extend NeonSkill class (#24)
    * Update skills to extend NeonSkill class
    
    * Update for Mycroft compat.

--- __init__.py
+++ __init__.py
@@ -27,23 +27,27 @@ from pprint import pformat
 from dateutil.tz import gettz
 from datetime import datetime, timedelta
 from dateutil.parser import parse
-from tkinter import Tk
-from tkinter.filedialog import askopenfilename
 from adapt.intent import IntentBuilder
 from lingua_franca.format import nice_duration, nice_time, nice_date
 from lingua_franca.parse import extract_datetime, extract_duration
 from lingua_franca.parse import extract_number
-from lingua_franca import load_language
 
 # from NGI.utilities.configHelper import NGIConfig
 from json_database import JsonStorage
 from mycroft import Message
-from mycroft.util.log import LOG
-from mycroft.skills.core import MycroftSkill
+# from mycroft.util.log import LOG
+# from mycroft.skills.core import MycroftSkill
 from mycroft.util import play_audio_file
 from mycroft.util import resolve_resource_file
-from neon_utils import stub_missing_parameters, skill_needs_patching
+# from neon_utils import stub_missing_parameters, skill_needs_patching
+from neon_utils.skills.neon_skill import NeonSkill, LOG
 
+try:
+    from lingua_franca import load_language
+    from tkinter import Tk
+    from tkinter.filedialog import askopenfilename
+except Exception as x:
+    LOG.error(x)
 WEEKDAY_NAMES = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
 
 
@@ -69,18 +73,23 @@ class AlertStatus(IntEnum):
     MISSED = 1
 
 
-class AlertSkill(MycroftSkill):
+class AlertSkill(NeonSkill):
 
     def __init__(self):
         super(AlertSkill, self).__init__(name="AlertSkill")
-        self.internal_language = "en"
-        load_language(self.internal_language)
+        try:
+            self.internal_language = "en"
+            load_language(self.internal_language)
+        except Exception as e:
+            # TODO: This is for Mycroft compat until they update LF DM
+            LOG.error(e)
         self.nlp = spacy.load("en_core_web_sm")
-        if skill_needs_patching(self):
-            stub_missing_parameters(self)
-            self.recording_dir = None
-        else:
-            self.recording_dir = os.path.join(self.configuration_available['dirVars']['docsDir'], "neon_recordings")
+        # if skill_needs_patching(self):
+        #     stub_missing_parameters(self)
+        #     self.recording_dir = None
+        # else:
+        self.recording_dir = os.path.join(self.configuration_available.get('dirVars', {})
+                                          .get('docsDir', os.path.expanduser("~/.neon")), "neon_recordings")
 
         # self.alerts_cache = NGIConfig("alerts", self.file_system.path)
         self.alerts_cache = JsonStorage(os.path.join(self.file_system.path, "alerts"))
@@ -954,10 +963,13 @@ class AlertSkill(MycroftSkill):
                 # TODO: Server file selection
             else:
                 self.speak_dialog("RecordingNotFound", private=True)
-                root = Tk()
-                root.withdraw()
-                file = askopenfilename(title="Select Audio for Alert", initialdir=self.recording_dir,
-                                       parent=root)
+                try:
+                    root = Tk()
+                    root.withdraw()
+                    file = askopenfilename(title="Select Audio for Alert", initialdir=self.recording_dir,
+                                           parent=root)
+                except Exception as e:
+                    LOG.error(e)
         return file
 
     def _get_alerts_for_user(self, user: str) -> dict:
