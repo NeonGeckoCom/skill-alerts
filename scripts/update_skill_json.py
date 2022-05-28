4f9805860185849d700e6381e083ec39b42c3120
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2022-05-27 17:58:41 -0700

    Refactor skill.json parsing to import method from neon_utils (#38)
    * Refactor skill.json parsing to import method from neon_utils
    
    * Fix typo in neon-utils version spec
    
    * fix neon-utils version spec
    
    Co-authored-by: Daniel McKnight <daniel@neon.ai>

--- scripts/update_skill_json.py
+++ scripts/update_skill_json.py
@@ -27,97 +27,28 @@
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import json
-import shutil
 from os.path import dirname, join
 from pprint import pprint
-
-from ovos_skills_manager.local_skill import get_skill_data_from_directory
-from neon_utils.file_utils import parse_skill_readme_file
-from neon_utils.configuration_utils import dict_merge
+from neon_utils.packaging_utils import build_skill_spec
 
 skill_dir = dirname(dirname(__file__))
-
-_invalid_skill_data_keys = ("appstore", "appstore_url", "credits",
-                            "skill_id")
-_invalid_readme_keys = ("contact support", "details")
-
-default_skill = {"title": "",
-                 "url": "",
-                 "summary": "",
-                 "short_description": "",
-                 "description": "",
-                 "examples": [],
-                 "desktopFile": False,
-                 "warning": "",
-                 "systemDeps": False,
-                 "requirements": {
-                     "python": [],
-                     "system": {},
-                     "skill": []
-                 },
-                 "incompatible_skills": [],
-                 "platforms": ["i386",
-                               "x86_64",
-                               "ia64",
-                               "arm64",
-                               "arm"],
-                 "branch": "master",
-                 "license": "",
-                 "icon": "",
-                 "category": "",
-                 "categories": [],
-                 "tags": [],
-                 "credits": [],
-                 "skillname": "",
-                 "authorname": "",
-                 "foldername": None}
-
-
-def get_skill_license():
-    with open(join(skill_dir, "LICENSE.md")) as f:
-        contents = f.read()
-    if "BSD-3" in contents:
-        return "BSD-3-Clause"
-    if "Apache License" in contents:
-        return "Apache 2.0"
-    if "Neon AI Non-commercial Friendly License 2.0" in contents:
-        return "Neon 2.0"
-    if "Neon AI Non-commercial Friendly License" in contents:
-        return "Neon 1.0"
 
 
 def get_skill_json():
     print(f"skill_dir={skill_dir}")
     skill_json = join(skill_dir, "skill.json")
-    backup = join(skill_dir, "skill_json.bak")
-    shutil.move(skill_json, backup)
-    skill_data = get_skill_data_from_directory(skill_dir)
-    shutil.move(backup, skill_json)
-    skill_data['foldername'] = None
-    for key in _invalid_skill_data_keys:
-        if key in skill_data:
-            skill_data.pop(key)
-    readme_data = parse_skill_readme_file(join(skill_dir, "README.md"))
-    for key in _invalid_readme_keys:
-        if key in readme_data:
-            readme_data.pop(key)
-    # readme_data["description"] = readme_data.pop("details")
-    readme_data["short_description"] = readme_data.get("summary")
-    readme_data["license"] = get_skill_license()
-    readme_data["branch"] = "master"
-    skill_data = dict_merge(default_skill, skill_data)
-    combined = dict_merge(skill_data, readme_data)
-    pprint(combined)
+    skill_spec = build_skill_spec(skill_dir)
+    pprint(skill_spec)
     try:
         with open(skill_json) as f:
             current = json.load(f)
     except Exception as e:
         print(e)
         current = None
-    if current != combined:
+    if current != skill_spec:
         print("Skill Updated. Writing skill.json")
         with open(skill_json, 'w+') as f:
-            json.dump(combined, f, indent=4)
+            json.dump(skill_spec, f, indent=4)
     else:
         print("No changes to skill.json")
 
