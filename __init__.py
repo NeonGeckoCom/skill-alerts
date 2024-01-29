365d101081cb3030aed74a200a04e58746bcadc8
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2024-01-29 15:24:13 -0800

    Update tests to latest automation/shared classes (#135)
    * Update tests to use shared test class
    
    * Replace relative imports with full package spec
    
    * Update imports in test class to match skill module
    
    * Update leftover imports in test class to match skill module
    
    ---------
    
    Co-authored-by: Daniel McKnight <daniel@neon.ai>

--- __init__.py
+++ __init__.py
@@ -47,16 +47,15 @@ from ovos_bus_client.message import Message
 from neon_utils.message_utils import request_from_mobile, dig_for_message
 from neon_utils.skills.neon_skill import NeonSkill
 from neon_utils.user_utils import get_user_prefs, get_message_user
+from ovos_workshop.decorators import intent_handler
 
-from mycroft.skills import intent_handler, intent_file_handler
-
-from .util import Weekdays, AlertState, MatchLevel, AlertPriority, WEEKDAYS, WEEKENDS, EVERYDAY
-from .util.alert import Alert, AlertType
-from .util.alert_manager import AlertManager, get_alert_id
-from .util.parse_utils import build_alert_from_intent, spoken_time_remaining, \
+from skill_alerts.util import Weekdays, AlertState, MatchLevel, AlertPriority, WEEKDAYS, WEEKENDS, EVERYDAY
+from skill_alerts.util.alert import Alert, AlertType
+from skill_alerts.util.alert_manager import AlertManager, get_alert_id
+from skill_alerts.util.parse_utils import build_alert_from_intent, spoken_time_remaining, \
     parse_alert_name_from_message, tokenize_utterance, \
     parse_alert_time_from_message
-from .util.ui_models import build_timer_data, build_alarm_data
+from skill_alerts.util.ui_models import build_timer_data, build_alarm_data
 
 
 class AlertSkill(NeonSkill):
@@ -393,7 +392,7 @@ class AlertSkill(NeonSkill):
             alerts_string = f"{alerts_string}\n{add_str}"
         self.speak(alerts_string, private=True)
 
-    @intent_file_handler('list_alerts.intent')
+    @intent_handler('list_alerts.intent')
     def alt_handle_list_alerts(self, message):
         """
         Intent handler for "what are my alerts", "are there any alerts", etc.
@@ -464,7 +463,7 @@ class AlertSkill(NeonSkill):
                 to_speak = f"{to_speak}\n{part}"
             self.speak(to_speak.lstrip('\n'), private=True)
 
-    @intent_file_handler("quiet_hours_start.intent")
+    @intent_handler("quiet_hours_start.intent")
     def handle_start_quiet_hours(self, message):
         """
         Handles starting quiet hours.
@@ -477,7 +476,7 @@ class AlertSkill(NeonSkill):
         self.speak_dialog("quiet_hours_start", private=True)
         self.update_skill_settings({"quiet_hours": True}, message)
 
-    @intent_file_handler("quiet_hours_end.intent")
+    @intent_handler("quiet_hours_end.intent")
     def handle_end_quiet_hours(self, message):
         """
         Handles ending quiet hours or requests for missed alerts.
@@ -982,6 +981,11 @@ class AlertSkill(NeonSkill):
         :param alert: expired Alert object
         """
         LOG.info(f'alert expired: {get_alert_id(alert)}')
+        # TODO: Emit generic event for remote clients
+        self.bus.emit(Message("neon.alert", alert.data, alert.context))
+        if alert.context.get("mq"):
+            LOG.warning("Alert from remote client; do nothing locally")
+            return
         self.make_active()
         self._gui_notify_expired(alert)
 
@@ -1043,6 +1047,7 @@ class AlertSkill(NeonSkill):
         while self.alert_manager.get_alert_status(alert_id) == \
                 AlertState.ACTIVE and time.time() < timeout:
             if alert_message.context.get("klat_data"):
+                # TODO: Deprecated
                 self.send_with_audio(self.dialog_renderer.render(
                     "expired_alert", {'name': alert.alert_name}),
                     to_play, alert_message, private=True)
@@ -1072,9 +1077,11 @@ class AlertSkill(NeonSkill):
             if alert.alert_type == AlertType.REMINDER:
                 self.speak_dialog('expired_reminder',
                                   {'name': alert.alert_name},
+                                  message=alert_message,
                                   private=True, wait=True)
             else:
                 self.speak_dialog('expired_alert', {'name': alert.alert_name},
+                                  message=alert_message,
                                   private=True, wait=True)
             self.make_active()
             time.sleep(10)
